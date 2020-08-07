58635e08c3e41d5fa9542b7d3bf46047ba4c0d3e
Zietieflr <lrmcguire93@gmail.com>
2020-08-07 17:11:48 -0600

    Add full functionality of calculator. Limited to 12 digits.

--- app/src/main/java/com/tutorials/tutorial3/MainActivity.kt
+++ app/src/main/java/com/tutorials/tutorial3/MainActivity.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.ArithmeticException
 
 class MainActivity : AppCompatActivity() {
 
@@ -44,6 +45,66 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    fun onEqual(view: View) {
+        if(lastNumeric) {
+            var tvValue = tvInput.text.toString()
+            var prefix = ""
+            try{
+                if(tvValue.startsWith("-")) {
+                    prefix = "-"
+                    tvValue = tvValue.substring(1)
+                }
+
+                if(tvValue.contains("-")) {
+                    val splitValue = tvValue.split("-")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if(!prefix.isEmpty()){
+                        one = prefix + one
+                    }
+
+                    tvInput.text = removeZeroAfterDot((one.toDouble() - two.toDouble()).toString())
+                }
+                if(tvValue.contains("+")) {
+                    val splitValue = tvValue.split("+")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if(!prefix.isEmpty()){
+                        one = prefix + one
+                    }
+
+                    tvInput.text = removeZeroAfterDot((one.toDouble() + two.toDouble()).toString())
+                }
+                if(tvValue.contains("/")) {
+                    val splitValue = tvValue.split("/")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if(!prefix.isEmpty()){
+                        one = prefix + one
+                    }
+
+                    tvInput.text = removeZeroAfterDot((one.toDouble() / two.toDouble()).toString())
+                }
+                if(tvValue.contains("*")) {
+                    val splitValue = tvValue.split("*")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if(!prefix.isEmpty()){
+                        one = prefix + one
+                    }
+
+                    tvInput.text = removeZeroAfterDot((one.toDouble() * two.toDouble()).toString())
+                }
+            }catch (e: ArithmeticException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
     private fun isOperatorAdded(value: String) : Boolean {
         return if (value.startsWith("-")){
             false
@@ -51,5 +112,12 @@ class MainActivity : AppCompatActivity() {
             value.contains("/") || value.contains("*") ||
                     value.contains("+") || value.contains("-")
         }
+    }
+
+    private fun removeZeroAfterDot(result: String) : String {
+        var value = ""
+        if(result.contains(".0"))
+            value = result.substring(0, result.length - 2)
+        return value
     }
 }
